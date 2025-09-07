f50be7d392858ce8174e4417ad48135299f68d31
Prem <pprakash312003@gmail.com>
2025-09-07 21:58:42 +0530

    2

--- admin.js
+++ admin.js
@@ -1,29 +1,24 @@
-const {kafka } = require('kafkajs');
+const { kafka } = require("./client");
 
-const kafka = new kafka({
-    clientId:"my-app",
-    brokers: ["https://192.168.1.6:9092"],
+async function init() {
+  const admin = kafka.admin();
+  console.log("Admin connecting...");
+  admin.connect();
+  console.log("Adming Connection Success...");
 
-})
+  console.log("Creating Topic [rider-updates]");
+  await admin.createTopics({
+    topics: [
+      {
+        topic: "rider-updates",
+        numPartitions: 2,
+      },
+    ],
+  });
+  console.log("Topic Created Success [rider-updates]");
 
-async function init(){
-    
-const admin = kafka.admin()
-    console.log('Admin connecting...')
-    admin.connect();
-    console.log("Admin Connection Success");
-
-    await admin.createTopics({
-        validateOnly: <boolean>,
-        waitForLeaders: <boolean>
-        timeout: <Number>,
-        topics: <ITopicConfig[]>,
-    })
-    
-
+  console.log("Disconnecting Admin..");
+  await admin.disconnect();
 }
 
-
-
-
-
+init();
